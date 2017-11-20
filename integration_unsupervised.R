69570710d26c2e08f627890c5d868540aab3cdbf
nalcala <nalcala@stanford.edu>
2017-11-20 16:09:56 +0100

    debug script

--- integration_unsupervised.R
+++ integration_unsupervised.R
@@ -24,21 +24,73 @@ Kmax  = as.numeric(opt$Kmax)
 cores = as.numeric(opt$cores)
 
 require(iClusterPlus)
-
+require(gplots)
+require(lattice)
 # read data
-if(!is.null(opt$DNA)) Ddata = read.table(opt$DNA,h=T)
-if(!is.null(opt$RNA)) Rdata = read.table(opt$RNA,h=T)
-if(!is.null(opt$methylation)) Mdata = read.table(opt$Methylation,h=T)
-if(!is.null(opt$CNV)) Cdata = read.table(opt$CNV,h=T)
+Ddata = NULL
+Cdata = NULL
+Rdata = NULL
+Mdata = NULL
 
-# tune lambda
-cv.fitl = vector("list",5)
-for(k in (Kmin:Kmax-1) ){
-  cv.fitl[[k]] = tune.iClusterPlus(cpus=cores,dt1=t(Ddata),dt2=t(Cdata),dt3=t(Rdata),dt4=t(Mdata), type=c("binomial","gaussian","gaussian","gaussian"), n.lambda=NULL,K=k,maxiter=20)
-  cv.fittmp = cv.fitl[[k]]
-  save(cv.fittmp, file=paste(opt$out,"cv.fit.k",k,".Rdata",sep=""))
+
+dtl = vector("list",4)
+ntypes = 0
+type = c()
+row.order = c()
+plot.chr  = c()
+sparse    = c()
+cap       = c()
+if(!is.null(opt$DNA)){
+  ntypes = ntypes +1
+  Ddata = read.table(opt$DNA,h=T,row.names = 1)
+  dtl[[ntypes]] = t(Ddata)
+  type = c(type,"binomial")
+  row.order = c(row.order,T)
+  plot.chr  = c(plot.chr,F)
+  sparse    = c(sparse,T)
+  cap       = c(cap,T)
+}
+if(!is.null(opt$CNV)){
+  ntypes = ntypes +1
+  dtl[[ntypes]] = t(Cdata)
+  type = c(type,"gaussian")
+  row.order = c(row.order,T)
+  plot.chr  = c(plot.chr,T)
+  sparse    = c(sparse,T)
+  cap       = c(cap,T)
+}
+if(!is.null(opt$RNA)){
+  ntypes = ntypes +1
+  Rdata = read.table(opt$RNA,h=T,row.names = 1,check.names = T)
+  dtl[[ntypes]] = t(Rdata)
+  type = c(type,"gaussian")
+  row.order = c(row.order,T)
+  plot.chr  = c(plot.chr,F)
+  sparse    = c(sparse,T)
+  cap       = c(cap,T)
+}
+if(!is.null(opt$methylation)){
+  ntypes = ntypes +1
+  Mdata = read.table(opt$methylation,h=T,row.names = 1)
+  dtl[[ntypes]] = t(Mdata)
+  type = c(type,"gaussian")
+  row.order = c(row.order,T)
+  plot.chr  = c(plot.chr,F)
+  sparse    = c(sparse,T)
+  cap       = c(cap,T)
 }
 
+print(dtl)
+print(type)
+
+# tune lambda
+cv.fitl = vector("list",Kmax-Kmin+1)
+for(k in (Kmin:Kmax-1) ){
+  cv.fitl[[k]] = tune.iClusterPlus(cpus=cores,dt1=dtl[[1]],dt2=dtl[[2]],dt3=dtl[[3]],dt4=dtl[[4]], type=type, n.lambda=NULL,K=k,maxiter=20)
+  cv.fittmp = cv.fitl[[k]]
+  save(cv.fittmp, file=paste(opt$out,"cv.fit.K",k+1,".Rdata",sep=""))
+}
+warnings()
 nLambda = nrow(cv.fitl[[1]]$lambda)
 nK = length(cv.fitl)
 BIC = getBIC(cv.fitl)
@@ -53,46 +105,64 @@ for(i in 1:nK){
 #best.fitl=sapply( Kmin:Kmax-1 , function(k) cv.fitl[[k]]$fit[[which.min(BIC[,k])]] )
 clusters=getClusters(cv.fitl)
 colnames(clusters)=paste("K=",Kmin:Kmax,sep="")
+clusters2 = clusters
+k = Kmin
+while(k<Kmax){
+  print(k)
+  clusters2 = clusters
+  for(i in 1:max(clusters[,k-1]) ){
+    print(i)
+    tatmp = table(clusters[clusters[,k-1]==i,k])
+    newid = as.numeric(names(tatmp)[which.max(tatmp)])
+    clusters2[clusters[,k]==newid,k] = i
+    clusters2[clusters[,k]==i,k] = newid
+    print(clusters2)
+    clusters = clusters2
+  }
+  k = k+1
+}
 
-
-svg(paste(out,"iClusters.svg",sep=""),h=6,w=8*2)
-par(family="Times",las=1,mfrow=c(1,2))
-plot(Kmin:Kmax,devRatMinBIC,type="b",xlab="K",ylab="Pseudo R2")
-image(cbind(clusters),col=prettycolors,axes=T,xlab="",ylab="K")
-#axis(1,at = 1:50,labels=rep("",50) )
-#axis(2,at = 2:6,las=1)
+svg(paste(opt$out,"iClusters.svg",sep=""),h=6,w=8*2)
+par(family="Times",mfrow=c(1,2))
+plot(Kmin:Kmax,devRatMinBIC,type="b",xlab=expression(italic(K)),ylab=expression("Pseudo "~italic(R^2)),las=1)
+image(1:nrow(clusters),Kmin:Kmax,clusters,col=rainbow(Kmax),axes=F,xlab="",ylab=expression(italic(K)),las=2 )
+axis(1,at = 1:nrow(clusters),labels = rownames(dtl[[1]]) ,las=2)
+axis(2,at = Kmin:Kmax,las=1)
 dev.off()
 
-# plot heatmap
+# plot heatmap and write features
+features = lapply(dtl,colnames)
 for(k in (Kmin:Kmax-1) ){
+  print(k)
   best.cluster=clusters[,k]
+  print(best.cluster)
   best.fit=cv.fitl[[k]]$fit[[which.min(BIC[,k])]]
+  print(best.fit)
   col.scheme = alist()
   col.scheme[[1]] = bluered(256)
   col.scheme[[2]] = bluered(256)
   col.scheme[[3]] = bluered(256)
   col.scheme[[4]] = bluered(256)
 
-  png(paste("Heatmap_K",k+1,".png",sep=""),h=4*200,w=4*300,bg=0)
-  plotHeatmap(fit=best.fit,datasets=list(mutations=t(Ddata),CNV=t(Cdata),RNA=t(Rdata),Methylation=t(Mdata)), type=c("binomial","gaussian","gaussian","gaussian"), col.scheme = col.scheme, row.order=c(T,T,T,T),plot.chr=c(F,T,F,F),sparse=c(T,T,T,T),cap=c(T,T,T,T))
+  png(paste(opt$out,"Heatmap_K",k+1,".png",sep=""),h=4*200,w=4*300,bg=0)
+  plotHeatmap(fit=best.fit,datasets=dtl[1:ntypes], type=type, col.scheme = col.scheme[1:ntypes], row.order=row.order,plot.chr=plot.chr,sparse=sparse,cap=cap)
   dev.off()
+  
+  # get best features
+  sigfeatures=alist()
+  idsigfeatures=alist()
+  print("yop")
+  print(sum(!sapply(features,is.null)))
+  for(i in 1:sum(!sapply(features,is.null)) ){
+    print(i)
+    rowsum=apply(abs(best.fit$beta[[i]]),1, sum)
+    upper=quantile(rowsum,prob=0.75)
+    sigfeatures[[i]]=(features[[i]])[which(rowsum>upper)]
+    idsigfeatures[[i]]=which(rowsum>upper)
+    write.table(sigfeatures[[i]],file = paste(opt$out,"_K",k+1,"_features",i,".txt",sep=""),col.names = F,quote = F)
+  }
+  #names(sigfeatures)=c("expression","methylation")
 }
 
-# get best features
-features = alist()
-features[[1]] = rownames(Ddata)
-features[[2]] = rownames(Cdata)
-features[[3]] = rownames(Rdata)
-features[[4]] = rownames(Mdata)
-sigfeatures=alist()
-idsigfeatures=alist()
-for(i in 1:4){
-  rowsum=apply(abs(best.fit$beta[[i]]),1, sum)
-  upper=quantile(rowsum,prob=0.75)
-  sigfeatures[[i]]=(features[[i]])[which(rowsum>upper)]
-  idsigfeatures[[i]]=which(rowsum>upper)
-}
-names(sigfeatures)=c("expression","methylation")
 
-write.table(sigfeatures,file = paste(out,"_features",sep=""))
 
